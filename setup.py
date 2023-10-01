0ace4185828b91b0f2a904d8805c6ef64d199e31
Pieter Provoost <pieterprovoost@gmail.com>
2023-10-01 21:15:12 +0200

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pqv",
-    version="0.6.1",
+    version="0.7.0",
     author="Pieter Provoost",
     author_email="pieterprovoost@gmail.com",
     description="Simple parquet viewer",
